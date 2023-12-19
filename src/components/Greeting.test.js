920f9281e0e5fbb104ab6b3803d6e8fd121eb14f
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-12-19 18:42:58 +0330

    Running a First Test_1

---
+++ src/components/Greeting.test.js
@@ -0,0 +1,13 @@
+import Greeting from "./Greeting";
+import { render, screen } from "@testing-library/react";
+test("renders Hello World as a text", () => {
+  //Arange
+  render(<Greeting />);
+
+  //Act
+  //...nothing
+
+  //Assert
+  const helloWorldElement = screen.getByText("Hello World");
+  expect(helloWorldElement).toBeInTheDocument();
+});
