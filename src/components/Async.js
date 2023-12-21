ea7d5c92d3df37822c8dbdb7340466424103926c
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-12-21 19:46:50 +0330

    add comment

--- src/components/Async.js
+++ src/components/Async.js
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const Async = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    //dummy data
+    fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((data) => {
         setPosts(data);
