2e9da57d8eed48cb344b2ff9032bc096b8f5d6c9
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-12-24 23:25:08 +0330

    adding comment

--- src/components/Async.js
+++ src/components/Async.js
@@ -4,7 +4,7 @@ const Async = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    //dummy data url
+    //dummy data url POSTs
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((data) => {
